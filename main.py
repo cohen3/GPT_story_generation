f58d6b67ae8eca019658b7412dc8bd461a748822
Elad <cohen3@post.bgu.ac.il>
2023-02-23 16:03:24 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# import pandas as pd
+# import cv2
+import openai
+# from transformers import AutoTokenizer, OpenAIGPTModel
+# import torch
+
+openai.api_key = "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"
+
+if __name__ == '__main__':
+    pass
+

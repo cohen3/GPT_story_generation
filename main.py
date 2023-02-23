2c11cd461ad114886611ec1b4d03ab8ef9f30281
Elad <cohen3@post.bgu.ac.il>
2023-02-23 16:09:52 +0200

    initial commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import openai
 # from transformers import AutoTokenizer, OpenAIGPTModel
 # import torch
 
-openai.api_key = "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"
+openai.api_key = ""
 
 if __name__ == '__main__':
     pass
